af878cdf9f2fb45d8a056af1c97f5762f6051b08
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-13 15:52:31 -0300

    feat: basic Main component implementation working

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+
+import api from '../../services/api'
 
 import styles from './styles';
 
@@ -9,6 +10,19 @@ import styles from './styles';
 import logo from '../../assets/logo.png';
 
 export default class Main extends Component {
+  state = {
+    newBox: '',
+  };
+
+  handleSignIn = async () => {
+    const response = await api.post('boxes', {
+      title: this.state.newBox,
+    });
+
+    // Navigates user to Box route
+    this.props.navigation.navigate('Box');
+  };
+
   render() {
     return (
       <View style={ styles.container }>
@@ -20,8 +34,10 @@ export default class Main extends Component {
           autoCapitalize="sentences"
           autoCorrect={false}
           underlineColorAndroid="transparent"
+          value={this.state.newBox}
+          onChangeText={text => this.setState({newBox: text})}
         />
-        <TouchableOpacity onPress={ () => {} } style= { styles.button }>
+        <TouchableOpacity onPress={this.handleSignIn} style= { styles.button }>
          <Text style={ styles.buttonText }>Create</Text>
         </TouchableOpacity>
       </View>
