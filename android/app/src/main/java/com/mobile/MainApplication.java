c023aa11b56bc2beff8826a8d5209d65a19d59b0
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-13 15:58:08 -0300

    feat: added image-picker dependency

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.rnfs.RNFSPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNFSPackage(),
             new VectorIconsPackage(),
             new AsyncStoragePackage(),
