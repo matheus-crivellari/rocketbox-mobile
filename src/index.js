51cb408109e057a2de9aecb2cc364bf60cf64ebf
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-13 19:45:39 -0300

    feat: YellowBox for ignoring some warnings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
+
+// Avoid in app warnings in development environment
+// due to websocket connections
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 import Routes from "./routes";
 
